e1485bb4648cbd48db5c5f7bc191b731d7f6ce00
Caleb Rodriguez <sirdigo6@gmail.com>
2021-12-15 13:27:57 -0500

    commit tree 4da9aebfc3ec170d4f544f0d0611cf5f2c72d65d

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [addItem, setAddItem] = useState(false);
+  const itemStatus = addItem ? "in-cart" : "";
+
+  function addAndRemove(){
+    setAddItem(addItem => !addItem)
+  }
+
   return (
-    <li className="">
+    <li className={itemStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addAndRemove}>{addItem ? "Remove From" : "Add To"} Cart</button>
     </li>
   );
 }
